4a6fc0f39787d7760f242a3c2976298c1a7f6558
Bilal05476 <ba4762313@gmail.com>
2021-11-28 12:37:19 +0500

    updates

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,7 +4,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "e2613f6d31mshe47f808ab8856acp191748jsn79bf85bb41cb",
+      "x-rapidapi-key": {process.env.RAPIDAPI-KEY},
     },
   });
   return data;
