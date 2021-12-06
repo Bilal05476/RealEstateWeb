c45188ffde9fd2f41db1591cd947f831cf5e8243
Bilal05476 <ba4762313@gmail.com>
2021-12-06 17:48:56 +0500

    updates

--- component/SearchFilter.jsx
+++ component/SearchFilter.jsx
@@ -12,9 +12,33 @@ import {
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, filterGetValues } from "../utils/filterData";
 
 const SearchFilter = () => {
-  return <div>SearchFilter</div>;
+  const [filter, setFilters] = useState(filterData);
+  const searchProperties = (filterValues) => {};
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filter.map((fil) => (
+        <Box key={fil.queryName}>
+          <Select
+            placeholder={fil.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) =>
+              searchProperties({ [fil.queryName]: e.target.value })
+            }
+          >
+            {fil?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilter;
